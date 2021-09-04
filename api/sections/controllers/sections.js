4ca82bbd3798579caa335642b8214cc9b929c5fc
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2021-09-04 07:39:17 -0300

    feature: customize section controller

--- api/sections/controllers/sections.js
+++ api/sections/controllers/sections.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    // let entities;
+
+    const entities = await strapi.services.sections.find(ctx.query);
+
+    return entities.map((entity) => ({
+      columns: entity.column,
+      locale: entity.locale,
+      id: entity.id,
+    }));
+  },
+};
